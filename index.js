59924c9b83034b3ab6316bcdb407a906e55d2950
Pavan kalyan C <35896290+pavankalyan-codes@users.noreply.github.com>
2022-04-12 22:13:42 +0530

    Added booking availability by theatre feature

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ app.get("/notify/:location/:title/:code/:telegramId", async (req, res, next) => 
     res.json({"bookingAvailable":!!available});
 });
 
+app.get("/notify/:theatre/:movie/:location/:locationCode/:movieCode/:telegramId", async (req, res, next) => {
+    const params=req.params;
+    const available=await checkTheatreForMovie(params.theatre,params.movie,params.location,params.locationCode,params.movieCode)
+    if(available){
+      sendMessage(`Booking Opened for ${params.title.toUpperCase()} 🎬 ✅✅✅ in ${theatre}`,params.telegramId);
+    }
+    res.json({"bookingAvailable":!!available});
+});
+
 
 async function sendMessage(message,chatId){
     const resp=await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,{
@@ -34,6 +43,17 @@ async function sendMessage(message,chatId){
 
 async function checkBooking(location,title,code){
     const res=await axios.get(`https://bms-booking.vercel.app/bms/${location}/${title}/${code}`)
+    .then((response)=> {
+        return response.data.bookingAvailable;
+    })
+    .catch(function (error) {
+        return false;
+    })
+    return res;
+}
+
+async function checkTheatreForMovie(theatre,movie,location,locationCode,movieCode){
+    const res=await axios.get(`https://bms-booking.vercel.app/${theatre}/${movie}/${location}/${locationCode}/${movieCode}`)
     .then((response)=> {
         return response.data.bookingAvailable;
     })
